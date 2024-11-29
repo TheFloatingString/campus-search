589891e74afcbdaff8c1c554c24f3333e5525e5f
Laurence Liang <laurence.w.liang@gmail.com>
2024-11-29 00:28:54 -0500

    Add UX fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,17 +33,19 @@ export default function Home() {
               if (e.key === "Enter") {
                 console.log("User pressed enter!");
                 e.preventDefault();
-
                 updateMessage(currUserPrompt);
               }
             }}
             type="search"
             id="default-search"
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg"
-            placeholder="Search for anything on campus."
+            placeholder="Feel free to search for events on campus!"
             required
           />
           <button
+            onClick={(e) => {
+              updateMessage(currUserPrompt);
+            }}
             type="submit"
             className="text-white absolute end-2.5 bottom-2.5 bg-blue-7 00 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
@@ -53,7 +55,6 @@ export default function Home() {
       </div>
       <div id="pageContent">
         <ReactMarkdown children={modelResp} remarkPlugins={[remarkGfm]} />
-        {/* <p>{modelResp}</p> */}
       </div>
     </main>
   );
