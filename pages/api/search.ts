7df9623254c7ca857e008e7deddac69fdb594f7a
Laurence Liang <laurence.w.liang@gmail.com>
2024-11-29 11:09:19 -0500

    Add event

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -24,7 +24,7 @@ export default async function handler(
         {
           role: "system",
           content:
-            'These are events happening at McGill. { "event": "Animal Therapy", "date": "2024-12-03", "time": "13:00 to 14:30", "location": "Student Services, McGill University" }, { "event": "Skills for Strategic Procrastination", "date": "2024-12-03", "time": "14:00 to 15:30", "location": "Student Services, McGill University" }, { "event": "McGill Noon-Hour Organ Series", "date": "2024-12-04", "time": "12:30 to 13:30", "location": "Redpath Hall, McGill University" }, { "event": "McGill Percussion Ensemble", "date": "2024-12-04", "time": }. Please be sure to return these events in a nice format: **Name of event** - **Date** - 5-word description.',
+            'These are events happening at McGill. { "event": "Animal Therapy", "date": "2024-12-03", "time": "13:00 to 14:30", "location": "Student Services, McGill University" }, { "event": "Skills for Strategic Procrastination", "date": "2024-12-03", "time": "14:00 to 15:30", "location": "Student Services, McGill University" }, { "event": "McGill Noon-Hour Organ Series", "date": "2024-12-04", "time": "12:30 to 13:30", "location": "Redpath Hall, McGill University" }, {"event": "CréaCinq by Douze à Deux", "date": "2024-11-30", "time": "17:00 to 19:00", "location": "Building 21, McGill University"} { "event": "McGill Percussion Ensemble", "date": "2024-12-04", "time": }. Please be sure to return these events in a nice format: **Name of event** - **Date** - 5-word description.',
         },
         { role: "user", content: req.body.content },
       ],
